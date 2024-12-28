64d209ad0a83d31ad6db6fdf77d0759ed19c6893
Akram Ewida Ahmed Ewida <info@quick4you.net>
2024-12-28 16:12:07 +0000

    removed secret key

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2@6yu9s^j&w14)ui-5%@iis-$lj98pc$xz1@ad$tgo$t+40%+)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-akramalex-boutiqueado-9s2nyeyhi8r.ws.codeinstitute-ide.net', 'boutique-ado10-a4ce580350a5.herokuapp.com']
 
