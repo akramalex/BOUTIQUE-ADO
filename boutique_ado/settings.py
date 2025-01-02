70b6023d0c674ee4b7e7f05fdb2d5782ea46b436
Akram Ewida Ahmed Ewida <info@quick4you.net>
2025-01-02 17:28:50 +0000

    added storages loctions

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -190,9 +190,13 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    STORAGES = {
+         "default": {
+                 "BACKEND": "custom_storages.MediaStorage",
+         },
+         "staticfiles": {"BACKEND": "custom_storages.StaticStorage"},
+}
     MEDIAFILES_LOCATION = 'media'
 
     # Override static and media URLs in production
