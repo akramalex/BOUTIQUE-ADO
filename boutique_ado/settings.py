d184059cfc0f9c8a75d273cac42eb8c0ec7de86d
Akram Ewida Ahmed Ewida <info@quick4you.net>
2025-01-02 16:27:25 +0000

    add files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,11 +182,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado10.s3-website.eu-north-1'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado10'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = 'boutique-ado10.s3-website.eu-north-1.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
