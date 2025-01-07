bd640c4bc4026a80dfedcfef088d8f9cbdee90fa
Akram Ewida Ahmed Ewida <info@quick4you.net>
2025-01-07 23:28:53 +0000

    added 404

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,3 +1,5 @@
+from .views import handler404
+
 """boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +30,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = 'boutique_ado.views.handler404'
